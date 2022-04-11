e397e55d5347dced0efda956d41a5b6e3e33d45c
Fuminori Tsuchiya <tsuchiya@pparc.gp.tohoku.ac.jp>
2022-04-11 13:22:58 +0900

    minor update

--- main.py
+++ main.py
@@ -19,15 +19,23 @@ while True:
     #----------------------------------------------------------
     # get one HF pachet
     #----------------------------------------------------------
-    st_hdr_pri, st_hdr_sec, st_hdr_rpw, st_aux, st_hdr_hf, st_cnt, hf_buff, hf_sz = hf_hdr.get_one_packet(fin)
+    st_hdr_pri, \
+    st_hdr_sec, \
+    st_hdr_rpw, \
+    st_aux,     \
+    st_hdr_hf,  \
+    st_cnt,     \
+    hf_buff,    \
+    hf_sz = hf_hdr.get_one_packet(fin)
 
 
     # Detect End-of-file
     if st_cnt.eof:
         break
 
-    print(format(st_hdr_pri.pid, '02x'),format(st_hdr_pri.pcat, '02x'))
-    print(hf_sz)
+    print('process ID      ',format(st_hdr_pri.pid, '02x'))
+    print('packet category ',format(st_hdr_pri.pcat, '02x'))
+    print('packet length   ',hf_sz)
 
     #----------------------------------------------------------
     # Convert L0 to L1
